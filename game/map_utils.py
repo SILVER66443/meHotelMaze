64ff913e17bee9746d2edc3a842883fbce32ccbe
silver <594846313@qq.com>
2020-11-27 15:56:36 +0800

    墙体碰撞

--- game/map_utils.py
+++ game/map_utils.py
@@ -35,8 +35,8 @@ class Map():
     def move(self):
         new_seeker = [self.seeker[0] + self.step[0], self.seeker[1] + self.step[1]]
 
-        if new_seeker[0] < 0 or new_seeker[0] > self.size[0] or new_seeker[1] < 0 or new_seeker[
-            1] > self.size[1]:
+        if new_seeker[0] < 0 or new_seeker[0] >= self.size[0] or new_seeker[1] < 0 or new_seeker[
+            1] >= self.size[1]:
             return [0, 0]
         if self.maze[new_seeker[0], new_seeker[1]] == 0:
 
